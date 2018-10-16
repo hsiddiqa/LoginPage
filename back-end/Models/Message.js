d64f82576b4914cb2fe884cdd54cb8518c36aa9b
Humaira Siddiqa <hsiddiqa@Humairas-MacBook-Pro.local>
2018-10-16 19:12:46 -0400

    user login page

---
+++ back-end/Models/Message.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('Message', {
+    msg: String,
+
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+    }
+});
---
+++ back-end/controllers/message.js
@@ -0,0 +1,21 @@
+var Message = require('../Models/Message');
+
+
+module.exports = {
+    get: function (req, res) {
+        Message.find({}).populate('user', '-pwd').exec(function (err, result) {
+            res.send(result);
+        })
+    },
+
+
+    post: function (req, res) {
+        console.log(req.body, req.user);
+        
+        req.body.user = req.user;
+        
+        var message = new Message(req.body);
+        message.save();
+        res.status(200);
+    }
+}
